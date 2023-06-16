85ff9ad496f69e2c5f6f6ba71827813bdd7047c8
Irrumabo <ballenstev@gmail.com>
2023-06-15 21:50:40 -0700

    added some logic to the code

---
+++ src/components/Section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import '../styles/Section.css';
+// add a style attribute to the section
+
+function Section() {
+  return (  <section className='section'>
+        {/* add in new stuff :) */}
+    </section>)
+}
+export default Section;
